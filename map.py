1abb8b4542de9fe0c62d1e5d5475e2fb9a772879
neeryks <akashdeep.singh49@gmail.com>
2022-06-11 22:50:15 +0530

    Updated Total Indian Population StateWise

--- map.py
+++ map.py
@@ -2,11 +2,12 @@ import plotly.express as px
 import pandas as pd
 import json
 import numpy as np
+from pyparsing import col
 
 
-india_map = json.load(open('INDIA_STATES.json','r'))
+india_map = json.load(open('states_india.geojson','r'))
 df = pd.read_excel("data.xlsx")
-df_s = pd.DataFrame({'Stats':df['Unnamed: 7'][3:38]},)
+df_s = pd.DataFrame({'Stats':df['Unnamed: 8'][3:38]},)
 #print(df_s)
 df = pd.concat([df['Unnamed: 1'][3:38],df_s],axis=1,ignore_index=True)
 df.reset_index(drop=True,inplace=True)
@@ -16,17 +17,18 @@ state_id_json = []
 state_name_json = []
 
 for st in india_map['features']:
-    state_name_json.append(st['properties']['STATE'])
-    state_id_json.append(st['_id'])
+    state_name_json.append(st['properties']['st_nm'])
+    #state_id_json.append(st['_id'])
 
-state_name_json = list(map(lambda x:x.upper(),state_name_json))
-df['State'] = list(map(lambda x:x.upper(),df['State']))
-state_id = pd.DataFrame({"State":state_name_json,"_id":state_id_json}).sort_values(by=['State'])
+#state_name_json = list(map(lambda x:x.upper(),state_name_json))
+#df['State'] = list(map(lambda x:x.upper(),df['State']))
+state_id = pd.DataFrame({"State":state_name_json}).sort_values(by=['State'])
 #print(state_id,df)
 df = pd.merge(df,state_id,on='State')
+df = df.append({"State":"Telangana","Stat":35003},ignore_index=True)
 print(df)
 
-fig = px.choropleth(df,geojson=india_map,locations='_id',color='Stat',featureidkey='_id',scope='asia')
+fig = px.choropleth(df,geojson=india_map,locations='State',color='Stat',featureidkey="properties.st_nm",scope='asia')
 fig.update_geos(fitbounds="locations", visible=False)
 fig.show()
 
