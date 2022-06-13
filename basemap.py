ef84901a2f707dc805f9a38a50a323833afdf0a9
neeryks <akashdeep.singh49@gmail.com>
2022-06-13 12:27:10 +0530

    Teriminal Operation

---
+++ basemap.py
@@ -0,0 +1,21 @@
+import json
+import pandas as pd
+import plotly.express as px
+import json
+
+class geoindiastate:
+    def __init__(self,file_Csv_etc,reference_to_map_with,What_to_plot_for,Name_of_map) -> None:
+
+        self.file = file_Csv_etc
+        self.geo_file = json.load(open("india_statenew.geojson","r"))
+        self.loc = reference_to_map_with
+        self.col = What_to_plot_for
+        self.name = Name_of_map
+    def showmap(self):
+        fig = px.choropleth(self.file,geojson=self.geo_file,locations=self.loc,featureidkey="properties.ST_NM",color=self.col,template="seaborn")
+        fig.update_geos(fitbounds="locations", visible=False)
+        fig.update_layout(height=800,title_text= self.name)
+        fig.update_traces(marker_line_width=1, marker_opacity=1,marker_line_color = "white")
+        return fig.show()
+    
+
---
+++ mapmaker.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from basemap import geoindiastate as gis
+from tkinter import filedialog as fd
+import time
+
+
+print("Select The File ")
+time.sleep(3)
+file = fd.askopenfilename()
+file_ext = file.split(".")[-1]
+
+print(f'You selected a "{file_ext}" file')
+
+ext_dict = {
+            "csv":pd.read_csv,
+            "xlsx":pd.read_excel,
+            "json":pd.read_json,
+            "sql":pd.read_sql}
+df = ext_dict[file_ext](file)
+attl = []
+att_list = ["reference column name","plot for column name","name of map"]
+for attributes in att_list:
+    att = input(f'Enter {attributes}: ')
+    attl.append(att)
+fig = gis(df,attl[0],attl[1],attl[2])
+fig.showmap()
+
+
+
