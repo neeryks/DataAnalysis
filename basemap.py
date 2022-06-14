14c1bb71bf6e345bf925e07761aa0dce9c34fde5
neeryks <akashdeep.singh49@gmail.com>
2022-06-14 10:43:14 +0530

    Improving Access

--- basemap.py
+++ basemap.py
@@ -4,19 +4,19 @@ import plotly.express as px
 import json
 
 class geoindiastate:
-    def __init__(self,file_Csv_etc,reference_to_map_with,What_to_plot_for,Name_of_map) -> None:
+    def __init__(self,file_Csv_etc,Name_of_map_or_graph) -> None:
 
         self.file = file_Csv_etc
         self.geo_file = json.load(open("geojsondata/india_statenew.geojson","r"))
-        self.loc = reference_to_map_with
-        self.col = What_to_plot_for
-        self.name = Name_of_map
-    def showmap(self):
-        fig = px.choropleth(self.file,geojson=self.geo_file,locations=self.loc,featureidkey="properties.ST_NM",color=self.col,template="seaborn")
+        self.name = Name_of_map_or_graph
+    def showmap(self,reference_to_map_with,What_to_plot_for):
+        fig = px.choropleth(self.file,geojson=self.geo_file,locations=reference_to_map_with,featureidkey="properties.ST_NM",color=What_to_plot_for,template="seaborn")
         fig.update_geos(fitbounds="locations", visible=False)
         fig.update_layout(height=800,title_text= self.name)
         fig.update_traces(marker_line_width=1, marker_opacity=1,marker_line_color = "white")
         return fig.show()
-
+    def barplot(self,xaxis,yaxis):
+        fig = px.bar(self.file,x=xaxis,y=yaxis)
+        return fig.show()
     
 
--- mapmaker.py
+++ mapmaker.py
@@ -1,3 +1,4 @@
+from turtle import end_fill
 import pandas as pd
 from basemap import geoindiastate as gis
 from tkinter import filedialog as fd
@@ -18,15 +19,21 @@ ext_dict = {
             "sql":pd.read_sql}
 df = ext_dict[file_ext](file)
 attl = []
-print("Column Names :")
-for x in df.columns:
-    print(x)
-att_list = ["reference column name","plot for column name","name of map"]
-for attributes in att_list:
-    att = input(f'Enter {attributes}: ')
-    attl.append(att)
-fig = gis(df,attl[0],attl[1],attl[2])
-fig.showmap()
+for x in enumerate(df.columns):
+    print(f'{x[0]}: {x[1]}',end="\t")
+print("<====CHOOSE FROM COLUMN NAMES")
+choice = input(f'Plot-type: ')
+if choice == "map":
+    att_list = ["reference column name","plot for column name","name of map"]
+    for attributes in att_list:
+        att = input(f'Enter {attributes}: ')
+        attl.append(att)
+    fig = gis(df,attl[2])
+    fig.showmap(attl[0],attl[1])
+elif choice == "bar":
+    wrd = input("Enter name of map ")
+    fig = gis(df,wrd)
+    fig.barplot(input("Enter X Axis "),input("Enter Y Axis "))
 
 
 
